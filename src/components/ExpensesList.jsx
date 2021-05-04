f7d09e8e8fb3112b28752b1c55c757dd5ca45c57
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2021-05-04 20:47:53 +0500

    Add searching

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { ExpenseItem } from './ExpenseItem'
 
 
 export function ExpensesList() {
   const { expenses, dispatch } = useContext(AppContext)
+  const [searchString, setSearchString] = useState('')
 
   const handleDelete = id => {
     dispatch({
@@ -13,12 +14,37 @@ export function ExpensesList() {
     })
   }
 
+  const handleSearch = (event) => {
+    setSearchString(event.target.value)
+  }
+
+  let displayExpenses = expenses
+  if (searchString.trim()) {
+    displayExpenses = expenses.filter(
+      ({name}) => name.toLowerCase().includes(searchString.trim().toLowerCase())
+    )
+  }
+
   return (
-    <ul className="list-group">
-      {expenses.map(expense => (
-        <ExpenseItem key={expense.id} {...expense} onDelete={handleDelete} />
-      ))}
-    </ul>
+    <>
+      <input
+        type="text"
+        name="search"
+        placeholder="Type to search..."
+        className="form-control mb-3"
+        value={searchString}
+        onChange={ handleSearch }
+      />
+      { displayExpenses.length > 0 ?
+        <ul className="list-group">
+          {displayExpenses.map(expense => (
+            <ExpenseItem key={expense.id} {...expense} onDelete={handleDelete} />
+          ))}
+        </ul>
+        :
+        <div className="alert alert-primary">Nothing found</div>
+      }
+    </>
   )
 }
 
