63fcb93e7f4500fda2b3091b51265a17612d905a
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2021-05-06 03:06:58 +0500

    Fix button

--- src/components/AddExpenseFrom.jsx
+++ src/components/AddExpenseFrom.jsx
@@ -36,39 +36,39 @@ export function AddExpenseFrom() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="row">
+    <form onSubmit={handleSubmit} className="col-md-7">
       { error && <div className="alert alert-danger">{ error }</div> }
-      <div className="row">
-        <div className="col-sm">
-          <label htmlFor="expense-form-name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="expense-form-name"
-            className="form-control"
-            required="required"
-            value={ name }
-            onChange={ e => setName(e.target.value) }
-          />
-        </div>
-        <div className="col-sm">
-          <label htmlFor="expense-form-cost">Cost</label>
-          <input
-            type="text"
-            name="cost"
-            id="expense-form-cost"
-            className="form-control"
-            required="required"
-            value={ cost }
-            onChange={ e => setCost(e.target.value) }
-          />
-        </div>
-        <div className="col-sm">
-          <button type="submit" className="btn btn-primary mt-2">
-            Submit
-          </button>
-        </div>
+      <div className="form-group">
+        <label htmlFor="expense-form-name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="expense-form-name"
+          className="form-control"
+          required="required"
+          value={ name }
+          onChange={ e => setName(e.target.value) }
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="expense-form-cost">Cost</label>
+        <input
+          type="text"
+          name="cost"
+          id="expense-form-cost"
+          className="form-control"
+          required="required"
+          value={ cost }
+          onChange={ e => setCost(e.target.value) }
+        />
+      </div>
+      <div className="form-group">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </div>
     </form>
+    </div>
   )
 }
